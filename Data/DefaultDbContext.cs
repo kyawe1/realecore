e56fa1cd267c9324babb9b7da54da48c92f0f99c
kyawe <kk09254010314@gmail.com>
2022-04-02 20:36:38 +0630

    changes

--- Data/DefaultDbContext.cs
+++ Data/DefaultDbContext.cs
@@ -16,13 +16,27 @@ public class DefaultDbContext : IdentityDbContext<ApplicationUser>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
+        string RoleId=Guid.NewGuid().ToString();
         builder.Entity<IdentityRole>().HasData(
             new IdentityRole(){
+                Id=RoleId,
                 Name="Admin",
                 NormalizedName="ADMIN"
             }
         );
+        string userId=Guid.NewGuid().ToString();
+        ApplicationUser user=new ApplicationUser{
+            Id=userId,
+            UserName="one@one.com",
+            Email="one@one.com",
+        };
+        IPasswordHasher<ApplicationUser> hasher=new PasswordHasher<ApplicationUser>();
+        user.PasswordHash=hasher.HashPassword(user,"123112");
+        builder.Entity<ApplicationUser>().HasData(user);
+        builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>(){
+            RoleId=RoleId,
+            UserId=userId
+        });
     }
     public DbSet<Interest> Interests { set; get; }
     public DbSet<House> houses { set; get; }
